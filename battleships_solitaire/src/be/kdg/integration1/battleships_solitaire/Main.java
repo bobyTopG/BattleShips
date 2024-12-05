86ecaffa4cc855b80a9f8bb8d49127ea3e2d340d
Teodor S. Patsov <teodor.patsov@student.kdg.be>
2024-12-05 15:43:23 +0000

    Revert "hints ready:9 REALY"
    This reverts commit 190369c85f642466a1c88afdc04569ef9263507b

---
+++ battleships_solitaire/src/be/kdg/integration1/battleships_solitaire/Main.java
@@ -0,0 +1,10 @@
+package be.kdg.integration1.battleships_solitaire;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+       GameController gameController = new GameController();
+       gameController.startGame(4);
+    }
+}
