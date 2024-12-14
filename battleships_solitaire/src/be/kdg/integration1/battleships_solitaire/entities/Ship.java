e3780fcb7899e90ad565a105ee35d17c8f11b10a
Teodor Patsov <jarousqui@icloud.com>
2024-12-14 16:47:08 +0100

    Formalized the ship and tile entities and put their corresponding enumerations inside.

--- battleships_solitaire/src/be/kdg/integration1/battleships_solitaire/entities/Ship.java
+++ battleships_solitaire/src/be/kdg/integration1/battleships_solitaire/entities/Ship.java
@@ -1,31 +1,103 @@
 package be.kdg.integration1.battleships_solitaire.entities;
 
-public class Ship {
-    private int x;  // Starting X coordinate
-    private int y;  // Starting Y coordinate
-    private ShipType shipType; // Type of ship
-    private boolean isVertical; // Orientation
+import be.kdg.integration1.battleships_solitaire.logic.Utility;
 
-    public Ship(int x, int y, ShipType shipType, boolean isVertical) {
+/**
+ * {@code Ship} is used to represent the ships in the game
+ * and when fetching them from the database.
+ */
+public class Ship {
+
+    /** Enumeration for the types of ships **/
+    public enum Type {
+        SUBMARINE(1),
+        DESTROYER(2),
+        CRUISER(3),
+        BATTLESHIP(4),
+        CARRIER(5);
+
+        private final int size;
+
+        Type(int size) {
+            this.size = size;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public static Type getFromSize(int size) {
+            for (Type type : Type.values())
+                if (type.getSize() == size) return type;
+            throw new IllegalArgumentException("Ship type not found for size: " + size);
+        }
+    }
+
+    private final int x;
+    private final char y;
+    private final Type type;
+    private final Boolean isVertical;
+
+    private int remainingParts;
+
+    public Ship(int x, char y, Type type, boolean isVertical) {
         this.x = x;
         this.y = y;
-        this.shipType = shipType;
+        this.type = type;
         this.isVertical = isVertical;
+        this.remainingParts = type.size;
+    }
+
+    public Ship(int x, char y, int size, boolean isVertical) {
+        this(x, y, Type.getFromSize(size), isVertical);
+    }
+
+    public Ship(int x, int y, Type type, boolean isVertical) {
+        this(x, Utility.convertCoordinate(y), type, isVertical);
+    }
+
+    public Ship(int x, int y, int size, boolean isVertical) {
+        this(x, y, Type.getFromSize(size), isVertical);
     }
 
     public int getX() {
         return x;
     }
 
-    public int getY() {
+    public char getY() {
         return y;
     }
 
     public int getSize() {
-        return shipType.getSize();
+        return type.size;
     }
 
     public boolean isVertical() {
         return isVertical;
+    }
+
+    public int getRemainingParts() {
+        return remainingParts;
+    }
+
+    public void incrementRemainingParts() {
+        if (remainingParts == type.size)
+            throw new IllegalStateException("Ship is already fully hidden");
+        remainingParts++;
+    }
+
+    public void decrementRemainingParts() {
+        if (remainingParts == 0)
+            throw new IllegalStateException("Ship already has no remaining parts");
+        remainingParts--;
+    }
+
+    @Override
+    public String toString() {
+        int foundParts = type.size - remainingParts;
+        return String.format("%s%s",
+                "#".repeat(foundParts),
+                "_".repeat(remainingParts)
+        );
     }
 }
--- battleships_solitaire/src/be/kdg/integration1/battleships_solitaire/entities/Tile.java
+++ battleships_solitaire/src/be/kdg/integration1/battleships_solitaire/entities/Tile.java
@@ -1,44 +1,91 @@
 package be.kdg.integration1.battleships_solitaire.entities;
 
+import be.kdg.integration1.battleships_solitaire.logic.Utility;
+
+/**
+ * {@code Tile} is used to represent the tiles in the board of the game
+ * and when fetching them from the database.
+ * Types:
+ * {@code WATER} means that the tile is NOT part of a ship &
+ * {@code SHIP_PART} means that the tile is part of a ship.
+ */
 public class Tile {
+
+    /** Enumeration for the types of ships **/
+    public enum Type {
+        WATER(true),
+        SHIP_PART(false);
+
+        private final boolean isShip;
+
+        Type(boolean isShip) {
+            this.isShip = isShip;
+        }
+
+        public boolean isShip() {
+            return isShip;
+        }
+
+        public Type getOpposite() {
+            return switch (this) {
+                case SHIP_PART -> WATER;
+                case WATER -> SHIP_PART;
+            };
+        }
+    }
+
     private int x;
-    private int y;
-    private boolean isShip;
-    private boolean isWater;
-    private boolean isHint;
+    private char y;
+    /** If it is {@code null}, then the tile is unmarked.**/
+    private Type type;
     private boolean isRevealed;
 
+    @Deprecated
+    private boolean isHint;
 
-
-
-    public Tile(int x, int y ) {
+    public Tile(int x, char y) {
         this.x = x;
         this.y = y;
+    }
 
+    public Tile(int x, int y) {
+        this.x = x;
+        this.y = Utility.convertCoordinate(y);
     }
 
     public boolean isRevealed() {
         return isRevealed;
     }
 
-    public void setRevealed(boolean revealed) {
-        isRevealed = revealed;
+    public void setRevealedAs(Type type) {
+        if (type == null)
+            throw new IllegalArgumentException("Revealed tile type cannot be null");
+        isRevealed = true;
+        this.type = type;
     }
 
     public int getX() {
         return x;
     }
 
-    public int getY() {
+    public char getY() {
         return y;
     }
 
+    public Type getType() {
+        return type;
+    }
+
     public boolean isShip() {
-        return isShip;
+        if (type == null) return false;
+        return type.isShip;
     }
+
     public boolean isWater() {
-        return isWater;
+        if (type == null) return false;
+        return !type.isShip;
     }
+
     public boolean isHint() {
         return isHint;
     }
@@ -47,10 +94,18 @@ public class Tile {
         this.isHint = isHint;
     }
 
-    public void setShip(boolean isShip) {
-        this.isShip = isShip;
+    public void markAs(Type type) {
+        if (isRevealed)
+            throw new IllegalStateException("Tile is already revealed");
+        this.type = type;
     }
-    public void setWater(boolean isWater) {
-        this.isWater = isWater;
+
+    @Override
+    public String toString() {
+        return switch (type) {
+            case WATER -> isRevealed ? "≈" : "~";
+            case SHIP_PART -> isRevealed ? "#" : "X";
+            case null -> " ";
+        };
     }
 }
