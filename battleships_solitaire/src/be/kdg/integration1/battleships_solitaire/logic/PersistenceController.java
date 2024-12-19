498c7b7666dbbe8d92579a8fb73b62c1c66d1fcb
vii-kii <vivian.kinner@student.kdg.be>
2024-12-19 17:46:18 +0100

    changed createTables method according to create script

--- battleships_solitaire/src/be/kdg/integration1/battleships_solitaire/logic/PersistenceController.java
+++ battleships_solitaire/src/be/kdg/integration1/battleships_solitaire/logic/PersistenceController.java
@@ -41,76 +41,80 @@ public class PersistenceController {
             Statement statement = connection.createStatement();
             statement.executeUpdate("""
                     CREATE TABLE IF NOT EXISTS players (
-                                        player_id INTEGER
-                                        GENERATED ALWAYS AS IDENTITY
-                                        CONSTRAINT pk_player_id PRIMARY KEY,
-                                        name VARCHAR(12)
-                                        CONSTRAINT nn_player_name NOT NULL
-                                        CONSTRAINT uq_player_name UNIQUE
-                                        CONSTRAINT ch_player_name
-                                        CHECK ( name = UPPER(name) ),
-                                        birthdate DATE
-                                        CONSTRAINT nn_birthdate NOT NULL
-                                        CONSTRAINT ch_birthdate
-                                        CHECK ( birthdate < NOW() ),
-                                        join_date DATE DEFAULT CURRENT_DATE
-                                        CONSTRAINT nn_join_date NOT NULL)
+                        player_id INTEGER
+                        GENERATED ALWAYS AS IDENTITY
+                        CONSTRAINT pk_player_id PRIMARY KEY,
+                        name VARCHAR(12)
+                        CONSTRAINT nn_player_name NOT NULL
+                        CONSTRAINT uq_player_name UNIQUE
+                        CONSTRAINT ch_player_name
+                        CHECK ( name = UPPER(name) ),
+                        birthdate DATE
+                        CONSTRAINT nn_birthdate NOT NULL
+                        CONSTRAINT ch_birthdate
+                        CHECK ( birthdate < NOW() ),
+                        join_date DATE DEFAULT CURRENT_DATE
+                        CONSTRAINT nn_join_date NOT NULL)
                     """);
             statement.executeUpdate("""
                     CREATE TABLE IF NOT EXISTS games (
-                                         game_id INTEGER
-                                             GENERATED ALWAYS AS IDENTITY
-                                             CONSTRAINT pk_game_id PRIMARY KEY,
-                                         start TIMESTAMP(0)
-                                             CONSTRAINT nn_start NOT NULL,
-                                         "end" TIMESTAMP(0),
-                                         duration INTERVAL(0)
-                                             CONSTRAINT nn_duration NOT NULL,
-                                         score NUMERIC(3)
-                                             CONSTRAINT nn_score NOT NULL,
-                                         board_size NUMERIC(2)
-                                             CONSTRAINT nn_board_size NOT NULL
-                                             CONSTRAINT ch_board_size
-                                                 CHECK ( board_size IN (5, 7, 9) ),
-                                         player_id INTEGER
-                                             CONSTRAINT nn_player_id NOT NULL
-                                             CONSTRAINT fk_player_id REFERENCES games(game_id)
-                                             ON DELETE SET NULL)
+                         game_id    INTEGER
+                         GENERATED ALWAYS AS IDENTITY
+                         CONSTRAINT pk_game_id PRIMARY KEY,
+                         start      TIMESTAMP(0)
+                         CONSTRAINT nn_start NOT NULL,
+                         "end"      TIMESTAMP(0),
+                         duration   INTERVAL(0)
+                         CONSTRAINT nn_duration NOT NULL,
+                         score      NUMERIC(3)
+                         CONSTRAINT nn_score NOT NULL,
+                         board_size NUMERIC(2)
+                         CONSTRAINT nn_board_size NOT NULL
+                         CONSTRAINT ch_board_size
+                         CHECK ( board_size IN (5, 7, 9) ),
+                         player_id  INTEGER
+                         CONSTRAINT nn_player_id NOT NULL
+                         CONSTRAINT fk_player_id REFERENCES players (player_id)
+                         ON DELETE SET NULL)
                     """);
             statement.executeUpdate("""
                     CREATE TABLE IF NOT EXISTS tiles (
-                                        tile_id INTEGER
-                                            GENERATED ALWAYS AS IDENTITY
-                                            CONSTRAINT pk_tile_id PRIMARY KEY,
-                                        x NUMERIC(2)
-                                            CONSTRAINT nn_x NOT NULL,
-                                        y NUMERIC(2)
-                                            CONSTRAINT nn_y NOT NULL,
-                                        is_ship BOOLEAN,
-                                        game_id INTEGER
-                                            CONSTRAINT nn_game_id NOT NULL
-                                            CONSTRAINT fk_tile_game_id REFERENCES games(game_id)
-                                            ON DELETE CASCADE)
+                        tile_id INTEGER
+                        GENERATED ALWAYS AS IDENTITY
+                        CONSTRAINT pk_tile_id PRIMARY KEY,
+                        x       NUMERIC(2)
+                        CONSTRAINT nn_x NOT NULL,
+                        y       CHAR(1)
+                        CONSTRAINT nn_y NOT NULL
+                        CONSTRAINT ch_y CHECK ( y = UPPER(y) ),
+                        is_ship BOOLEAN,
+                        is_revealed BOOLEAN
+                        CONSTRAINT nn_is_revealed NOT NULL,
+                        game_id INTEGER
+                        CONSTRAINT nn_game_id NOT NULL
+                        CONSTRAINT fk_tile_game_id REFERENCES games (game_id)
+                        ON DELETE CASCADE,
+                        CONSTRAINT uq_tile UNIQUE (tile_id, x, y))
                     """);
             statement.executeUpdate("""
                     CREATE TABLE IF NOT EXISTS ship_types (
-                                      ship_type_id INTEGER
-                                          GENERATED ALWAYS AS IDENTITY
-                                          CONSTRAINT pk_ship_type_id PRIMARY KEY,
-                                      length NUMERIC(1)
-                                          CONSTRAINT nn_length NOT NULL,
-                                      name VARCHAR(12)
-                                          CONSTRAINT nn_ship_name NOT NULL)
+                          ship_type_id INTEGER
+                              GENERATED ALWAYS AS IDENTITY
+                              CONSTRAINT pk_ship_type_id PRIMARY KEY,
+                          length NUMERIC(1)
+                              CONSTRAINT nn_length NOT NULL,
+                          name VARCHAR(12)
+                              CONSTRAINT nn_ship_name NOT NULL)
                     """);
             statement.executeUpdate("""
                     CREATE TABLE IF NOT EXISTS ships (
-                                       ship_id INTEGER
-                                           GENERATED ALWAYS AS IDENTITY
-                                           CONSTRAINT pk_ship_id PRIMARY KEY,
-                                       ship_type_id INTEGER
-                                           CONSTRAINT nn_ship_type_id NOT NULL
-                                           CONSTRAINT fk_ship_type_id REFERENCES ship_types(ship_type_id)
-                                           ON DELETE SET NULL)
+                       ship_id INTEGER
+                           GENERATED ALWAYS AS IDENTITY
+                           CONSTRAINT pk_ship_id PRIMARY KEY,
+                       ship_type_id INTEGER
+                           CONSTRAINT nn_ship_type_id NOT NULL
+                           CONSTRAINT fk_ship_type_id REFERENCES ship_types(ship_type_id)
+                           ON DELETE SET NULL)
                     """);
             statement.executeUpdate("""
                     CREATE TABLE IF NOT EXISTS placements (
